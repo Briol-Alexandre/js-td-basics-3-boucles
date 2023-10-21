13c0399967aa7b40a25a7052a2ffed9fc5001c3a
Briol Alexandre <alexandre.briol@student.hepl.be>
2023-10-21 17:19:54 +0200

    fin ex 4

--- starter/b01-for-e4.js
+++ starter/b01-for-e4.js
@@ -25,3 +25,8 @@
     - que se passe-t-il si vous avez déclaré i avec let ?
     - que se passe-t-il si vous déclarez i avec var ?
 */
+const valMult = parseInt(prompt('Voici les 10 premières valeurs de la table de multiplication par …'));
+for (let i = 1; i <= 10; i ++) {
+    console.log(`${i}*${valMult}=${i*valMult}`);
+
+}
